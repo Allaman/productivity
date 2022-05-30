3d4222a8474a3f4e4c059d47ea4fee6332e67e5f
Michael <michaeljohannpeter@gmail.com>
2022-05-30 20:29:09 +0200

    update generated html

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 # https://github.com/jgm/pandoc/wiki/Using-pandoc-to-produce-reveal.js-slides
 
 #pandoc -t revealjs -s -o docs/index.html presentation.md -V revealjs-url=reveal.js --css=custom.css --slide-level=2 --self-contained
-#pandoc -t revealjs -s -o docs/index.html presentation.md -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/ --css=custom.css --slide-level=2 --self-contained
-pandoc -t revealjs -s -o docs/index.html presentation.md -V revealjs-url=https://unpkg.com/reveal.js@4.1.0/ --slide-level=2 --self-contained
+# pandoc -t revealjs -s -o docs/index.html presentation.md -V revealjs-url=https://unpkg.com/reveal.js@3.9.2/ --css=custom.css --slide-level=2 --self-contained
+pandoc -t revealjs -s -o docs/index.html presentation.md -V revealjs-url=https://unpkg.com/reveal.js@4.1.0/ --css=custom.css --slide-level=2 --self-contained
